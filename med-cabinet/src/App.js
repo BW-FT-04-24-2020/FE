2a639d84a0ba91236cbac177b6a67af662a6c8c7
Henry Nelson <Henry.Nelson89@gmail.com>
2020-04-27 15:41:34 -0700

    implimemnted some basic styleing

--- med-cabinet/src/App.js
+++ med-cabinet/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './App.css';
+import './Login.css'
 
-import {BrowserRouter as Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Route, Switch } from 'react-router-dom'
 import Login from './components/Login'
 
 
 function App() {
   return (
-    <Switch>
-      <Route exact path= "/">
-        <Login/>
-      </Route>
-    </Switch>
-      
-    
+    <div className='App'>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </div>
+
+
+
 
   );
 }
