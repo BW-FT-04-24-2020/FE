09192326e3769435f2a9c4542f1a3fdb05f381fd
Henry Nelson <Henry.Nelson89@gmail.com>
2020-04-27 11:01:32 -0700

    set up the login form and started on cards and the header

---
+++ med-cabinet/src/components/Login.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+
+const Login = () => {
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+    })
+
+    return (
+        <>
+            <h1>MedCabinet</h1>
+            <form>
+                <label htmlFor='name' name='name' >Name*</label>
+                <input
+                    type='text'
+                    name='name'
+                    value={user.name}
+                />
+                <br />
+                <label htmlFor='email' name='email' >email*</label>
+                <input
+                    type='text'
+                    name='email'
+                    value={user.name}
+                />
+                <br/>
+                <label htmlFor='password' name='password' >Password*</label>
+
+                <input
+                    type='password'
+                    name='password'
+                    value={user.password}
+                />
+                <br/>
+            </form>
+        </>
+    )
+}
+export default Login
