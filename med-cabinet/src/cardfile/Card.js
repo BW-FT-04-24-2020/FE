08186112abaf840aa4bb37e7f2e097f180417acd
alfredo villasenor <alfredo.villasenor.aof@gmail.com>
2020-04-27 17:38:13 -0700

    dont think i saved last time i commited

--- med-cabinet/src/cardfile/Card.js
+++ med-cabinet/src/cardfile/Card.js
@@ -3,35 +3,35 @@ import React, { useState } from 'react';
 
 const initialState = [
     {
-    id: '',
-    race: '',
-    flavors: [''], /*made arrays for flavors and effects becasue they both have additional data */
-    effects: ['']
+        id: '',
+        race: '',
+        flavors: [''], /*made arrays for flavors and effects becasue they both have additional data */
+        effects: ['']
     }
-  ]
+]
 
 MainContainer = () => {
     const [cardData, setCardData] = useState(initialState);
 
 
-return (
-    <div>
-{
-            cardData.map(card =>
-                <div key={card.id}>
-                    {card.race}
-                    {card.flavors.map(value=>{
-                        console.log(value)
-                    })}
+    return (
+        <div>
+            {
+                cardData.map(card =>
+                    <div key={card.id}>
+                        {card.race}
+                        {card.flavors.map(value => {
+                            console.log(value)
+                        })}
 
-                    {card.effects.map(value=>{
-                        console.log(value)
-                    })}
+                        {card.effects.map(value => {
+                            console.log(value)
+                        })}
 
-                </div>)
-      }
-        
-    </div>
-)
-    }
+                    </div>)
+            }
+
+        </div>
+    )
+}
 export default Card;
