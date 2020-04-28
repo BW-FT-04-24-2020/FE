36f0f75dbc9a8a5d121325bb29ca08ee1c3f76d2
Michael Perez <mperez6473@gmail.com>
2020-04-28 14:22:41 -0500

    Added some basic card information to Top Strains and SavedStrains. SavedStrains is a new file that pretty much the same as TopStrains. They are both now rendered on App.js

--- med-cabinet/src/components/HomePage.js
+++ med-cabinet/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './Header/Header';
 import TopStrains from './HomePageComponents/TopStrains';
+import SavedStrains from './HomePageComponents/SavedStrains';
 // Renders the link to '/home'
 const HomePage = () => {
     return (
@@ -10,6 +11,7 @@ const HomePage = () => {
             <Header />
             {/* What will be the app tiles that carasel */}
             <TopStrains />
+            <SavedStrains />
         </div>
     );
 };
--- med-cabinet/src/components/HomePageComponents/SavedStrains.js
+++ med-cabinet/src/components/HomePageComponents/SavedStrains.js
@@ -1,59 +1,54 @@
 import React, { useState } from 'react';
+import { CardSection, initialState } from './TopStrains';
 
-import styled from 'styled-components';
-
-export const initialState = {
-    Afpak: {
-        id: 1,
-        race: 'hybrid',
-        flavors: ['Earthy', 'Chemical', 'Pine'],
-        effect: {
-            positive: ['Relaxed', 'Hungry', 'Happy', 'Sleepy'],
-            negative: ['Dizzy'],
-            medical: [
-                'Depression',
-                'Insomnia',
-                'Pain',
-                'Stress',
-                'Lack of Appetite',
-            ],
-        },
-    },
-    African: {
-        id: 2,
-        race: 'sativa',
-        flavors: ['Spicy/Herbal', 'Pungent', 'Earthy'],
-        effects: {
-            positive: [
-                'Euphoric',
-                'Happy',
-                'Creative',
-                'Energetic',
-                'Talkative',
-            ],
-            negative: ['Dry Mouth'],
-            medical: ['Depression', 'Pain'],
-        },
-    },
-};
-
-export const CardSection = styled('section')`
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    overflow-x: hidden;
-    margin: 1% 4%;
-    div {
-        border: 1px solid black;
-        margin: 2%;
-    }
-`;
-
-const TopStrains = () => {
-    const [topStrains, setTopStrains] = useState(initialState);
+const SavedStrains = () => {
+    const [savedStrains, setTopStrains] = useState(initialState);
     return (
         <>
-            <h2>Top Strains</h2>
+            <h2>Saved Strains</h2>
             <CardSection>
+                <div>
+                    <h3>StrainName</h3>
+                    <p>StrainRace</p>
+                    <p>
+                        StrainText, description.StrainText, description.
+                        StrainText, description. StrainText, description.
+                        StrainText, description. StrainText, description.
+                        StrainText, description.
+                    </p>
+                    <h5>Effects</h5>
+                    <ul>Medical:</ul>
+                    <ul>Positive:</ul>
+                    <ul>Negative:</ul>
+                </div>
+                <div>
+                    <h3>StrainName</h3>
+                    <p>StrainRace</p>
+                    <p>
+                        StrainText, description.StrainText, description.
+                        StrainText, description. StrainText, description.
+                        StrainText, description. StrainText, description.
+                        StrainText, description.
+                    </p>
+                    <h5>Effects</h5>
+                    <ul>Medical:</ul>
+                    <ul>Positive:</ul>
+                    <ul>Negative:</ul>
+                </div>
+                <div>
+                    <h3>StrainName</h3>
+                    <p>StrainRace</p>
+                    <p>
+                        StrainText, description.StrainText, description.
+                        StrainText, description. StrainText, description.
+                        StrainText, description. StrainText, description.
+                        StrainText, description.
+                    </p>
+                    <h5>Effects</h5>
+                    <ul>Medical:</ul>
+                    <ul>Positive:</ul>
+                    <ul>Negative:</ul>
+                </div>
                 <div>
                     <h3>StrainName</h3>
                     <p>StrainRace</p>
@@ -142,4 +137,4 @@ const TopStrains = () => {
         </>
     );
 };
-export default TopStrains;
+export default SavedStrains;
